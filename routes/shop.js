736121901efdea9be07b18ceda627650a9f9262c
Andrey Dmitriev <andrydmit@gmail.com>
2020-11-26 00:43:03 +0200

    orders with mongoDb implemented

--- routes/shop.js
+++ routes/shop.js
@@ -16,12 +16,12 @@ router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
-// router.post('/cart-delete-Item', shopController.postCartDeleteProduct);
-//
-// router.post('/create-order', shopController.postOrder);
-//
-// router.get('/orders', shopController.getOrders);
-//
+router.post('/cart-delete-Item', shopController.postCartDeleteProduct);
+
+router.post('/create-order', shopController.postOrder);
+
+router.get('/orders', shopController.getOrders);
+
 // router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
