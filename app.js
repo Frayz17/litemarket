0ff39a4f73361cec9b06f4e25189cadfa8cf729a
Andrey Dmitriev <andrydmit@gmail.com>
2020-11-28 01:00:53 +0200

    starting work with sessions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const connectString = require('./util/connectString');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -19,6 +20,9 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({ secret: 'my secret', resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
   User.findById('5fc0143b0a27de1610650eee')
