b21958826191e7ae29bef0967a240a937b3ba2bc
Andrey Dmitriev <andrydmit@gmail.com>
2020-11-28 18:41:01 +0200

    using cookies and sessions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn = req.get('Cookie').trim().split('=')[1];
-  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -9,6 +9,21 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect('/');
+  User.findById('5fc0143b0a27de1610650eee')
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        console.error(err);
+        res.redirect('/');
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.error(err);
+    res.redirect('/');
+  });
 };
