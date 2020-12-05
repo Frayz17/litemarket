5bf384c8e20f47822bbe3373520160f797475271
Andrey Dmitriev <andrydmit@gmail.com>
2020-12-05 12:22:11 +0200

    express-validator was added. Signup validation was added.

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 const sendGridKey = require('../util/connectString').sendGridApiKey;
 const crypto = require('crypto');
+const { validationResult } = require('express-validator/check');
 
 const transporter = nodemailer.createTransport(
   sendgridTransport({
@@ -78,34 +79,35 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
 
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash('error', 'This email already exist.');
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPass) => {
-          const user = new User({
-            email,
-            password: hashedPass,
-            cart: { items: [] },
-          });
-          return user.save();
-        })
-        .then(() => {
-          res.redirect('/login');
-          return transporter.sendMail({
-            to: email,
-            from: 'andrydmitriev@ukr.net',
-            subject: 'Signup succeeded!',
-            html: '<h1>You successfully signed up</h1>',
-          });
-        })
-        .catch((err) => console.log(err));
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPass) => {
+      const user = new User({
+        email,
+        password: hashedPass,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then(() => {
+      res.redirect('/login');
+      return transporter.sendMail({
+        to: email,
+        from: 'andrydmitriev@ukr.net',
+        subject: 'Signup succeeded!',
+        html: '<h1>You successfully signed up</h1>',
+      });
     })
     .catch((err) => console.log(err));
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { check, body, validationResult } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -14,7 +16,36 @@ router.post('/reset', authController.postReset);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post(
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              'E-mail exist already, please pick a different one.'
+            );
+          }
+        });
+      }),
+    body(
+      'password',
+      'Please enter a password with only numbers and text an at least 5 and not more than 20 characters'
+    )
+      .isLength({ min: 5, max: 20 })
+      .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords have to match!');
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
