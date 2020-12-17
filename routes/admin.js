3f80403065317388c2ac55ef069aa18dbc1c5349
Andrey Dmitriev <andrydmit@gmail.com>
2020-12-17 23:08:58 +0200

    delete product refactored.

--- routes/admin.js
+++ routes/admin.js
@@ -42,6 +42,6 @@ router.get(
 router.post('/edit-product', isAuth, adminController.postEditProduct);
 
 // /admin/delete-product => POST
-router.post('/delete-product', isAuth, adminController.postDeleteProduct);
+router.delete('/product/:productId', isAuth, adminController.deleteProduct);
 
 module.exports = router;
