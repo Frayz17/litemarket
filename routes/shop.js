4a4b65d8734aad5ec9ee330e8b016e12366eeea0
Andrey Dmitriev <andrydmit@gmail.com>
2020-12-18 23:58:22 +0200

    checkout page added

--- routes/shop.js
+++ routes/shop.js
@@ -19,6 +19,8 @@ router.post('/cart-delete-Item', isAuth, shopController.postCartDeleteProduct);
 
 router.post('/create-order', isAuth, shopController.postOrder);
 
+router.post('/checkout', isAuth, shopController.getCheckout);
+
 router.get('/orders', isAuth, shopController.getOrders);
 
 router.get('/orders/:orderId', isAuth, shopController.getInvoice);
