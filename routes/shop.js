6f92861868291410a789bce1b3c34991ea594d31
Andrey Dmitriev <andrydmit@gmail.com>
2020-12-19 11:49:40 +0200

    stripe was connected

--- routes/shop.js
+++ routes/shop.js
@@ -17,9 +17,9 @@ router.post('/cart', isAuth, shopController.postCart);
 
 router.post('/cart-delete-Item', isAuth, shopController.postCartDeleteProduct);
 
-router.post('/create-order', isAuth, shopController.postOrder);
-
-router.post('/checkout', isAuth, shopController.getCheckout);
+router.get('/checkout', isAuth, shopController.getCheckout);
+router.get('/checkout/success', isAuth, shopController.getCheckoutSuccess);
+router.get('/checkout/cancel', isAuth, shopController.getCheckout);
 
 router.get('/orders', isAuth, shopController.getOrders);
 
